1f4bcac7b05338b7c2eeb45be6dc119a3867cea9
Jose Pacio <pacioj@uw.edu>
2018-10-14 17:18:00 -0700

    Added Unity project

---
+++ Assets/Readme.txt
@@ -0,0 +1,4 @@
+How to watch they flythrough:
+
+1. hit the play button at the center.
+2. enjoy
---
+++ Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour {
+
+    public Light tvLight;
+    public Vector2 flickerRange;
+    public float flickerRate;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("flicker", 0, flickerRate);
+	}
+
+    void flicker() {
+        tvLight.intensity = Random.Range(flickerRange.x, flickerRange.y);
+    }
+}
